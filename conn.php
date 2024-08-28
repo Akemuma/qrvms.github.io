9eae75dbc15e03dc4b3c5313017ac8c73a86ce7a
Akemuma <142207344+Akemuma@users.noreply.github.com>
2024-08-28 14:06:20 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,11 +1,2 @@
-<?php
 
-function connect()
-{
-    $conn = new mysqli("localhost", "root", "", "qrvisitor");
-    if (!$conn) die("Database is being upgrade!");
-    return $conn;
-}
-$conn = connect();
-if (!$conn) die("Under Construction!");
 
